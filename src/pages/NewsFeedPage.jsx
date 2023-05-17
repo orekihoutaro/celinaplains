00c09dfe1cc93938877b71aef272c49e38c79f0c
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-17 10:43:42 +0800

    Update scroll

--- src/pages/NewsFeedPage.jsx
+++ src/pages/NewsFeedPage.jsx
@@ -1,48 +1,59 @@
-import React from 'react'
-import { lazy, Suspense }  from 'react';
+import React from 'react';
+import { lazy, Suspense } from 'react';
 import Loader from '../components/Loader';
 const Navbar = lazy(() => import('../components/Navbar'));
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import bg from "../assets/bg-1.jpg"
 
 const NewsFeedPage = () => {
-    const [newsFeed, setNewsFeed] = useState([]);
+  const [newsFeed, setNewsFeed] = useState([]);
 
-    useEffect(() => {
-      fetchNewsFeed();
-    }, []);
-  
-    const fetchNewsFeed = async () => {
-      try {
-        const response = await axios.get('https://celinaplains-api.onrender.com/news-feed');
-        setNewsFeed(response.data);
-      } catch (error) {
-        console.error('Error fetching news feed:', error);
-      }
-    };
+  useEffect(() => {
+    fetchNewsFeed();
+  }, []);
+
+  const fetchNewsFeed = async () => {
+    try {
+      const response = await axios.get('https://celinaplains-api.onrender.com/news-feed');
+      setNewsFeed(response.data);
+    } catch (error) {
+      console.error('Error fetching news feed:', error);
+    }
+  };
+
   return (
     <Suspense fallback={<Loader />}>
-        <Navbar />
-        <div className="flex flex-col bg-background1 bg-cover pt-[100px] px-10">
-            <span className='font-poppins font-semibold text-2xl text-white'>
-                Celina Plains Imus<br/>
-            </span>
-            <div className="bg-white p-2 w-1/4">
-            <h1 className="text-2xl font-bold mb-4">News Feed</h1>
-            {newsFeed.map((item) => (
-                <div key={item.id} className="mb-4">
+      <img src={bg} alt="Background Image" className="absolute w-screen h-full bg-cover -z-20"/>
+      <Navbar />
+      <div className="flex flex-col h-screen pt-[100px] px-10">
+        <span className="text-2xl font-semibold text-white font-poppins">Celina Plains Imus</span>
+        <div className="w-1/4 p-4 overflow-y-auto bg-white rounded-xl">
+          <h1 className="mb-4 text-2xl font-bold">News Feed</h1>
+          {newsFeed.map((item) => (
+            <div key={item.id} className="flex mb-4">
+              {item.data && (
+                <img
+                  src={item.data}
+                  alt="News Feed"
+                  className="object-cover w-[450px] mr-4 rounded-lg"
+                />
+              )}
+              <div className="flex flex-col">
                 <h2 className="text-lg font-bold">{item.postTitle}</h2>
-                <p className="text-gray-500">{item.postCaption}</p>
-                <p className="text-gray-500">{new Date(item.createdAt).toLocaleString()}</p>
-                {item.data && (
-                    <img src={item.data} alt="News Feed" className="mt-2 w-[512px]" />
+                <p className="font-medium text-slate-800">{item.postCaption}</p>
+                {item.createdAt && (
+                  <p className="text-gray-500 text-md">
+                    {new Date(item.createdAt).toLocaleString()}
+                  </p>
                 )}
-                </div>
-            ))}
+              </div>
             </div>
+          ))}
         </div>
+      </div>
     </Suspense>
-  )
-}
+  );
+};
 
-export default NewsFeedPage
+export default NewsFeedPage;
